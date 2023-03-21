bcdc66bc4fdc8111dd8707e377099e7851ad1106
Felipealvesnn <felipe.alvesnn@gmail.com>
2023-03-20 22:43:23 -0300

    att

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,3 +8,10 @@ const generateId = () => {
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 };
+
+const scrollToBottom = (id) => {
+    const element = $(`#${id}`);
+    element.animate({
+        scrollTop: element.prop("scrollHeight")
+    }, 100);
+}

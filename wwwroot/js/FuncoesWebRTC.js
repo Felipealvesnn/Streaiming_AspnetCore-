587e9ca23470bf05dcc63af7c6291ee27d81669a
Felipe NoWork <felipe.rudra@hotmail.com>
2023-03-23 12:01:10 -0300

    aetareado bora ver o teste

--- wwwroot/js/FuncoesWebRTC.js
+++ wwwroot/js/FuncoesWebRTC.js
@@ -9,26 +9,8 @@ const acceptCall = () => {
 
 
 };
-const eventosPerl = ()=> {
-    peerConnection.onicecandidate = event => {
 
-        if (event.candidate) {
 
-            var targetUserConnectionId = users.filter(u => u.username != user);
-            console.info(`Target user: ${targetUserConnectionId[0].username}`);
-            setTimeout(() => {
-                hubConnection.invoke('sendData', JSON.stringify({ 'candidate': event.candidate }), targetUserConnectionId[0].connectionId).catch(err => console.error(err));
-            }, 1000);
-        }
-    };
-
-    peerConnection.addEventListener('connectionstatechange', event => {
-        if (peerConnection.connectionState === 'connected') {
-            alert("conectado")
-        }
-    });
-
-}
 const declineCall = () => {
     var callingUserName = $('#callmodal').attr('data-cid');
     hubConnection.invoke('AnswerCall', false, caller).catch(err => console.error(err));
@@ -55,22 +37,7 @@ const endCall = (connectionId) => {
 
 const ligarWebCam = () => {
 
-    //peerConnection.ontrack = event => {
-    //    const stream = event.streams[0];
-    //    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
-    //        remoteVideo.srcObject = stream;
-    //    }
-    //}; 
-    //// ,amdar a stream
-    //stream.getTracks().forEach(track => {
-    //    try {
-    //        peerConnection.addTrack(track, stream);
-    //    } catch (error) {
-    //        console.error(error);
-    //        // Display an error message to the user
-    //        //alert('There was an error adding the audio and video track. Please try again or contact support.');
-    //    }
-    //});
+
 
     
     navigator.mediaDevices.getUserMedia({
@@ -88,14 +55,84 @@ const ligarWebCam = () => {
     });
 
 }
+const callbackRemoveStream = (connection, evt) => {
+    console.log('WebRTC: removing remote stream from partner window');
+    // Clear out the partner window
+   
+    remoteVideo.src = '';
+}
+const callbackAddVideo = (connection, evt) => {
+    console.log('WebRTC: called callbackAddStream');
 
-const ConectWebRtc = (Caller, localStream) =>{
+    // Bind the remote stream to the partner window
+    //var otherVideo = document.querySelector('.video.partner');
+    //attachMediaStream(otherVideo, evt.stream); // from adapter.js
+    attachMediaStream(evt);
+}
+attachMediaStream = (e) => {
+    //console.log(e);
+    console.log("OnPage: called attachMediaStream");
+
+    const stream = e.streams[0];
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+        remoteVideo.srcObject = stream;
+    }
+};
+const callbackIceCandidate = (evt, connection, partnerClientId) => {
+    console.log("WebRTC: Ice Candidate callback");
+    //console.log("evt.candidate: ", evt.candidate);
+    if (evt.candidate) {// Found a new candidate
+        console.log('WebRTC: new ICE candidate');
+        //console.log("evt.candidate: ", evt.candidate);
+        sendHubSignal(JSON.stringify({ "candidate": evt.candidate }), partnerClientId);
+    } else {
+        // Null candidate means we are done collecting candidates.
+        console.log('WebRTC: ICE candidate gathering complete');
+        sendHubSignal(JSON.stringify({ "candidate": null }), partnerClientId);
+    }
+}
+const initializeConnection = (partnerClientId) => {
+    console.log('WebRTC: Initializing connection...');
+    
+    var connection = new RTCPeerConnection(peerConnectionConfig);
+
+   
+    connection.onicecandidate = evt => callbackIceCandidate(evt, connection, partnerClientId); // ICE Candidate Callback
+  
+    connection.onaddstream = evt => callbackAddVideo(connection, evt); // Add stream handler callback
+   // connection.onremovestream = evt => callbackRemoveStream(connection, evt); // Remove stream handler callback
+
+    connections[partnerClientId] = connection; // Store away the connection based on username
+    //console.log(connection);
+    return connection;
+}
+const getConnection = (partnerClientId) => {
+    console.log("WebRTC: called getConnection");
+    if (connections[partnerClientId]) {
+        console.log("WebRTC: connections partner client exist");
+        return connections[partnerClientId];
+    }
+    else {
+        console.log("WebRTC: initialize new connection");
+        return initializeConnection(partnerClientId)
+    }
+}
+
+sendHubSignal = (candidate, partnerClientId) => {
+    console.log('candidate', candidate);
+    console.log('SignalR: called sendhubsignal ');
+    hubConnection.invoke('sendData', candidate, partnerClientId).catch(errorHandler);
+};
+
+const ConectWebRtc = (partnerClientId, localStream) =>{
 
 
  
     console.log('WebRTC: called initiateoffer: ');
     var connection = getConnection(partnerClientId); // // get a connection for the given partner
-   
+    //console.log('initiate Offer stream: ', stream);
+    //console.log("offer connection: ", connection);
+    localStream.getTracks().forEach(track => connection.addTrack(track, stream));
     console.log("WebRTC: Added local stream");
 
     connection.createOffer().then(offer => {
@@ -117,4 +154,60 @@ const ConectWebRtc = (Caller, localStream) =>{
  
 
 
+}
+
+const receivedCandidateSignal = (connection, partnerClientId, candidate) => {
+   
+    console.log('WebRTC: adding full candidate');
+    connection.addIceCandidate(new RTCIceCandidate(candidate), () => console.log("WebRTC: added candidate successfully"), () => console.log("WebRTC: cannot add candidate"));
+   
+}
+// Recebi um novo sinal de conectao WebRTC
+const newSignal = (partnerClientId, data) => {
+    console.log('WebRTC: called newSignal');
+    //console.log('connections: ', connections);
+
+    var signal = JSON.parse(data);
+    var connection = getConnection(partnerClientId);
+    //console.log("signal: ", signal);
+    //console.log("signal: ", signal.sdp || signal.candidate);
+    //console.log("partnerClientId: ", partnerClientId);
+    console.log("connection: ", connection);
+
+    // Route signal based on type
+    if (signal.sdp) {
+        console.log('WebRTC: sdp signal');
+        receivedSdpSignal(connection, partnerClientId, signal.sdp);
+    } else if (signal.candidate) {
+        console.log('WebRTC: candidate signal');
+        receivedCandidateSignal(connection, partnerClientId, signal.candidate);
+    } else {
+        console.log('WebRTC: adding null candidate');
+        connection.addIceCandidate(null, () => console.log("WebRTC: added null candidate successfully"), () => console.log("WebRTC: cannot add null candidate"));
+    }
+}
+
+// Process a newly received SDP signal
+const receivedSdpSignal = (connection, partnerClientId, sdp) => {
+   
+    connection.setRemoteDescription(new RTCSessionDescription(sdp), () => {
+        console.log('WebRTC: set Remote Description');
+        if (connection.remoteDescription.type == "offer") {
+            console.log('WebRTC: remote Description type offer');
+            connection.addStream(localStream);
+            console.log('WebRTC: added stream');
+            connection.createAnswer().then((desc) => {
+                console.log('WebRTC: create Answer...');
+                connection.setLocalDescription(desc, () => {
+                    console.log('WebRTC: set Local Description...');
+                    console.log('connection.localDescription: ', connection.localDescription);
+                    //setTimeout(() => {
+                    sendHubSignal(JSON.stringify({ "sdp": connection.localDescription }), partnerClientId);
+                    //}, 1000);
+                }, errorHandler);
+            }, errorHandler);
+        } else if (connection.remoteDescription.type == "answer") {
+            console.log('WebRTC: remote Description type answer');
+        }
+    }, errorHandler);
 }
